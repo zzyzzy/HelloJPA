0aeda2d67d81737edc20b5d7b6d800b606993368
zzyzigy <zzyzigy@gmail.com>
2023-05-15 11:41:27 +0900

    jpa 연결 테스트

--- src/main/java/jpa/HelloJPA01.java
+++ src/main/java/jpa/HelloJPA01.java
@@ -1,4 +1,26 @@
 package jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class HelloJPA01 {
+    public static void main(String[] args) {
+        // jpa 객체 초기화 : emf -> em -> tx
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+
+    }
 }
